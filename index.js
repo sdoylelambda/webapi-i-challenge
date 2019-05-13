c98a1c63156e7f83c647b721f5867f3e2b84c494
Sean Doyle <sdoylelambda@gmail.com>
2019-05-13 16:26:21 -0400

    Post method.

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ server.get('/', (req, res) => {
     res.send('Hello from index.js');
 })
 
+server.post('/api/users', (req, res) => {
+    const apiInfo = req.body;
+    db.add(apiInfo)
+    .then(api => res.status(200).json(api))
+    .catch(err => res.status(400).json({ success: false, err }))
+})
+
 server.listen(4000, () => console.log(`\n*** Server running on http://localhost:4000 ***\n`));
