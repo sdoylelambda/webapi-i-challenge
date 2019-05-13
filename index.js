213577e0990f0ec333fd3e378eea84cfe8cd9de8
Sean Doyle <sdoylelambda@gmail.com>
2019-05-13 16:18:45 -0400

    Server running, get method.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send('Hello from index.js');
+})
+
+server.listen(4000, () => console.log(`\n*** Server running on http://localhost:4000 ***\n`));
